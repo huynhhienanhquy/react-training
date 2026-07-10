13c7d77097a766727324bbf83c1ee8a273799054
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-07-10 15:23:20 +0700

    feat: add sample product data

---
+++ src/hook-example5/data/products.ts
@@ -0,0 +1,32 @@
+import type { Product } from "../types/product";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Laptop Dell",
+    category: "Laptop",
+    price: 1200,
+    stock: 8,
+  },
+  {
+    id: 2,
+    name: "iPhone 15",
+    category: "Phone",
+    price: 999,
+    stock: 12,
+  },
+  {
+    id: 3,
+    name: "Samsung Monitor",
+    category: "Monitor",
+    price: 300,
+    stock: 5,
+  },
+  {
+    id: 4,
+    name: "Logitech Mouse",
+    category: "Accessory",
+    price: 40,
+    stock: 30,
+  },
+];
