f27d056a1d6709fc32959dce49c4d770b709200f
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-07-20 16:38:26 +0700

    chore(main): update application entry point and global imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './example1/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
