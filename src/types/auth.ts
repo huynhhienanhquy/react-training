0ab0d2e72365c62147b591c1dc973982d7a793e7
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-07-23 16:00:28 +0700

    feat: update interface user

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -5,12 +5,3 @@ export interface User {
   country?: string;
   isOnboarded?: boolean;
 }
-
-export interface AuthContextType {
-  user: User | null;
-  loading: boolean;
-  login: (email: string, password: string) => Promise<void>;
-  register: (email: string, password: string) => Promise<void>;
-  completeOnboarding: (fullName: string, country: string) => Promise<void>;
-  logout: () => void;
-}
