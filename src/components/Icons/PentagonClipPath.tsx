c58a1531cc0a075582dcf0e535e9635f5bb64421
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-08-03 20:54:25 +0700

    feat: update icon

--- src/components/Icons/PentagonClipPath.tsx
+++ src/components/Icons/PentagonClipPath.tsx
@@ -7,7 +7,15 @@ export const PentagonClipPath = () => {
           id="pentagon-clip"
           clipPathUnits="objectBoundingBox"
         >
-          <path d="M 0.5 0.05 C 0.52 0.05, 0.93 0.32, 0.95 0.35 C 0.97 0.38, 0.88 0.88, 0.85 0.92 C 0.82 0.96, 0.18 0.96, 0.15 0.92 C 0.12 0.88, 0.03 0.38, 0.05 0.35 C 0.07 0.32, 0.48 0.05, 0.5 0.05 Z" />
+          <polygon
+            points="
+              0.5,0.05
+              0.95,0.35
+              0.85,0.92
+              0.15,0.92
+              0.05,0.35
+            "
+          />
         </clipPath>
       </defs>
     </svg>
