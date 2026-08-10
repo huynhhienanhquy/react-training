486741c24aa5725c38aeaef2f6d282d6516ffb4c
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-08-10 20:57:32 +0700

    fix: use plural travel API endpoints

--- src/services/hotelService.ts
+++ src/services/hotelService.ts
@@ -3,13 +3,13 @@ import type { HotelData } from '../types/hotel';
 
 //Get a list of all hotels
 export const getHotels = async (): Promise<HotelData[]> => {
-  const response = await flightApi.get<HotelData[]>('/hotel');
+  const response = await flightApi.get<HotelData[]>('/hotels');
   return response.data;
 };
 
 
 //Get details of a hotel by ID.
 export const getHotelById = async (id: string): Promise<HotelData> => {
-  const response = await flightApi.get<HotelData>(`/hotel/${id}`);
+  const response = await flightApi.get<HotelData>(`/hotels/${id}`);
   return response.data;
 };
